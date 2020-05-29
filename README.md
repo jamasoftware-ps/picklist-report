acffdb6d80ced4d301f488a32e9cc829b9632e63
Nick McHale <39273258+nmchale-jama@users.noreply.github.com>
2020-05-29 08:57:14 -0700

    inital commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# Pick-list Report Generator
+The script will generate a report of the Pick-lists and the Pick-list options with their API ID's for your instance
+
+# Installation
+This section contains information on how to install the required dependencies for this script.
+
+## Pre-Requisites
+* [Python 3.7+](https://www.python.org/downloads/release/python-377/) If using pipenv you must use a python 3.7.X 
+version.  If installing requirements manually you may use any python version including 3.8+ however testing has only
+been done against python 3.7
+
+* [py-jama-rest-client](https://pypi.org/project/py-jama-rest-client/)
+
+* Enable the REST API on your Jama Connect instance
+
+## Pipenv installation (Recommended)
+If you do not already have Pipenv installed on your machine you can learn how to install it here: 
+[https://pypi.org/project/pipenv/](https://pypi.org/project/pipenv/)
+
+The required dependencies for this project are managed with Pipenv and can be installed by opening a terminal
+to the project directory and entering the following command:
+```bash
+    pipenv install
+```
+
+## Manual installation
+If you do not wish to use Pipenv you may manually install the required dependencies with pip.
+```bash
+pip install --user py-jama-rest-client
+```
+
+Or you can use the requiremnts.txt file as follows
+```bash
+pip install -r requirements.txt
+```
+
+# Usage
+This section contains information on configuration and execution the script.
+
+## Configuration
+Before you can execute the script you must configure the script via a config file.  The config file is
+structured in a standard .ini file format. there is an example config.ini file included with this repo that you
+may modify with your settings.  I recommend that you create a copy of the template config file and rename it to
+something that is meaningful for your execution.
+
+#### Client Settings:
+This section contains settings related to connecting to your Jama Connect REST API.
+
+* jama_connect_url: this is the URL to your Jama Connect instance
+
+* oauth: setting this value to 'false' will instruct the client to authenticate via basic authentication.  Setting this 
+value to 'true' instructs the client to use OAuth authentication protocols
+
+* user_id: This should be either your username or clientID if using OAuth
+
+* user_secret: This should be either your password or client_secret if using OAuth
+
+
+## Running the script
+
+1) Open a terminal to the project directory.
+2) If using pipenv enter the following(otherwise skip to step 3):
+   ```bash
+   pipenv shell 
+   ``` 
+3) Enter the following command into your terminal (Note that the script accepts one parameter and that is the path to
+the config file created above):  
+   ```bash 
+   python picklist-report-generator.py config.ini
+   ```
+
+## Output
+Execution logs will be output to the terminal as well as output to a log file in the logs/ folder located next to the 
+script.
---
+++ picklist-report-generator.py
@@ -0,0 +1,122 @@
+import os
+import sys
+import logging
+import datetime
+import configparser
+from typing import Callable, Union
+
+from py_jama_rest_client.client import JamaClient
+from py_jama_rest_client.client import APIException
+
+logger = logging.getLogger(__name__)
+
+
+def init_logging():
+    try:
+        os.makedirs('logs')
+    except FileExistsError:
+        pass
+    current_date_time = datetime.datetime.now().strftime("%Y-%m-%d %H_%M_%S")
+    log_file = 'logs/harm-severity-updater_' + str(current_date_time) + '.log'
+    logging.basicConfig(filename=log_file, level=logging.INFO)
+    logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
+
+def parse_config():
+    if len(sys.argv) != 2:
+        logger.error("Incorrect number of arguments supplied.  Expecting path to config file as only argument.")
+        exit(1)
+    current_dir = os.path.dirname(__file__)
+    path_to_config = sys.argv[1]
+    if not os.path.isabs(path_to_config):
+        path_to_config = os.path.join(current_dir, path_to_config)
+
+    # Parse config file.
+    configuration = configparser.ConfigParser()
+    configuration.read_file(open(path_to_config))
+    return configuration
+
+
+def create_jama_client(config: configparser.ConfigParser):
+    url = None
+    user_id = None
+    user_secret = None
+    oauth = None
+    try:
+        url = config.get('CLIENT_SETTINGS', 'jama_connect_url').strip()
+        # Clean up URL field
+        while url.endswith('/') and url != 'https://' and url != 'http://':
+            url = url[0:len(url) - 1]
+        # If http or https method not specified in the url then add it now.
+        if not (url.startswith('https://') or url.startswith('http://')):
+            url = 'https://' + url
+        oauth = config.getboolean('CLIENT_SETTINGS', 'oauth')
+        user_id = config.get('CLIENT_SETTINGS', 'user_id').strip()
+        user_secret = config.get('CLIENT_SETTINGS', 'user_secret').strip()
+    except configparser.Error as config_error:
+        logger.error("Unable to parse CLIENT_SETTINGS from config file because: {}, "
+                     "Please check config file for errors and try again."
+                     .format(str(config_error)))
+        exit(1)
+
+    return JamaClient(url, (user_id, user_secret), oauth=oauth)
+
+
+def generate_report(client: JamaClient):
+    # Get the pick-list info
+    try:
+        picklists = client.get_pick_lists()
+    except APIException as error:
+        logger.error("Failed to fetch pick-list data.")
+        logger.error(error)
+        return
+
+    # Open a File for writing
+    try:
+        with open("picklist-report.txt", "w") as report:
+            # process each picklist
+            for picklist in picklists:
+                # Print the Picklist info
+                pl_name = picklist.get('name')
+                pl_id = picklist.get('id')
+                report.write('======================================\n')
+                report.write('Pick-list:\n')
+                report.write('Name: ' + pl_name + '\n')
+                report.write('ID: ' + str(pl_id) + '\n')
+
+                # Fetch and print the picklist option info for this picklist
+                try:
+                    picklist_options = client.get_pick_list_options(pl_id)
+                    # Write each picklist option
+                    for pl_option in picklist_options:
+                        plo_name = pl_option.get('name')
+                        plo_id = pl_option.get('id')
+                        report.write('*****\n')
+                        report.write('\tPick-list Option:\n')
+                        report.write('\tName: ' + plo_name + '\n')
+                        report.write('\tID: ' + str(plo_id) + '\n')
+
+                except APIException as api_error:
+                    logger.error("Unable to fetch pick-list options")
+                    logger.error(api_error)
+
+    except IOError as file_error:
+        logger.error("Unable to write file")
+        logger.error(file_error)
+
+
+# Execute this as a script.
+if __name__ == "__main__":
+    # Setup logging
+    init_logging()
+
+    # Get Config File Path
+    conf = parse_config()
+
+    # Create Jama Client
+    jama_client = create_jama_client(conf)
+
+    # Begin business logic
+    generate_report(jama_client)
+
+    logger.info("Done.")
